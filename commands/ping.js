b65fb0fb817f5354d0c534e99bb91e12604ec664
Tanya575 <omankingomanking@gmail.com>
2021-03-31 15:29:25 +0400

    k

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'ping',
     aliases: ['p', 'بنق'],
-    permissions: ["ADMINISTRATOR"],
     description: "this is a ping command!",
     execute(client, message, args){
         message.channel.send('pong!');
