0b440e681b15d998e1ff3ee06a5b1c625e0cdebb
Tanya575 <omankingomanking@gmail.com>
2021-03-31 10:36:58 +0400

    ta

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ module.exports = (Discord, client) =>{
     console.log('Tanya is online!');
 
 
-    client.user.setActivity('Tanya sleeping room', { type: 'WATCHING' })
+    client.user.setActivity('commands | OMAR-ENJOY videos', { type: 'WATCHING' })
     .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
     .catch(console.error);
 }
