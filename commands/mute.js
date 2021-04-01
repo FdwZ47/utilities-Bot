facb4e817c414414708697f4f2d10c5053d932d1
Tanya575 <omankingomanking@gmail.com>
2021-04-01 21:12:52 +0400

    ooo

--- commands/mute.js
+++ commands/mute.js
@@ -3,7 +3,7 @@ const ms = require('ms');
 module.exports = {
     name: 'mute',
     aliases: ['m', 'اسكت', 'اص'],
-    cooldown: 10,
+    cooldown: 5,
     permissions: ["MUTE_MEMBERS"],
     description: "this mutes a member!",
     execute(client, message, args) {
