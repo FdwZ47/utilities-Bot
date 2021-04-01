9db7cce809c1844f3db525a042cf889dd5be5dbf
Tanya575 <omankingomanking@gmail.com>
2021-04-01 17:43:21 +0400

    ok

--- commands/suggetions.js
+++ commands/suggetions.js
@@ -3,7 +3,7 @@ module.exports = {
     aliases: ['suggest', 'suggestion'],
     permissions: [],
     description: 'creates a suggestion!',
-    execute(message, args, cmd, client, discord){
+    execute( client, message, args){
         const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
         if(!channel) return message.channel.send('suggestions channel does not exist!');
 
