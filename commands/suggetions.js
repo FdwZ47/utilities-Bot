1e8cee69e9f38b7b2beee3b5ec4415ba6cda6ef2
Tanya575 <omankingomanking@gmail.com>
2021-04-01 20:03:25 +0400

    ko

--- commands/suggetions.js
+++ commands/suggetions.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'suggestions',
     aliases: ['suggest', 'suggestion', 'اقترح'],
+    cooldown: 3600,
     permissions: [],
     description: 'creates a suggestion!',
     execute(client, message, args){
