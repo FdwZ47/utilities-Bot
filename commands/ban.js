9ab64e24b89e00842e23b6a31ea6d7aabf531795
Tanya575 <omankingomanking@gmail.com>
2021-04-01 09:22:26 +0400

    H

--- commands/ban.js
+++ commands/ban.js
@@ -8,7 +8,10 @@ module.exports = {
         if(member){
             const memberTarget = message.guild.members.cache.get(member.id);
             memberTarget.ban();
-            message.channel.send("<a:ench_pick:822221894195216385> that user has been banned");
+            const embed = new discord.MessageEmbed()
+            .setDescription(`<a:ench_pick:822221894195216385><@${memberTarget.user.id}> has been banned from the server`)
+            .setColor('#E30B5C')
+            message.channel.send(embed);
         }else{
             message.channel.send('<:idk:822217580567592991> I can t  find this member');
         }
