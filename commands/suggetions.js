f48683793e3df1aed9ab40dfd7dbff9c1a950062
Tanya575 <omankingomanking@gmail.com>
2021-04-01 18:40:26 +0400

    ok

--- commands/suggetions.js
+++ commands/suggetions.js
@@ -5,18 +5,18 @@ module.exports = {
     permissions: [],
     description: 'creates a suggestion!',
     execute(client, message, args){
-        const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
+        const channel = message.guild.channels.cache.find(c => c.name === '「📨」suggest');
         if(!channel) return message.channel.send('suggestions channel does not exist!');
 
         let messageArgs = args.join(' ');
         const embed = new Discord.MessageEmbed()
-        .setColor('FADF2E')
+        .setColor('RANDOM')
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
         .setDescription(messageArgs);
 
         channel.send(embed).then((msg) =>{
-            msg.react('👍');
-            msg.react('👎');
+            msg.react('<a:yes:822217053863673856>');
+            msg.react('<a:no:826693930678616104>');
             message.delete();
         }).catch((err)=>{
             throw err;
