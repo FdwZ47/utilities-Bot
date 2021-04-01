0836818b34ee9ac8e5acd30e376aed7e7024f744
Tanya575 <omankingomanking@gmail.com>
2021-04-01 17:06:14 +0400

    ty

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 module.exports = {
     name: 'avatar',
     aliases: ['icon', 'pfp', 'profilepic'],
@@ -7,11 +8,11 @@ module.exports = {
     execute(client, message, args) {
 
         if (!message.mentions.users.size) {
-            return message.channel.send(`**Your Avatar: ** ${message.author.displayAvatarURL({ dynamic: true })}`);
+            return message.channel.send(new Discord.MessageEmbed() .setDescription(`[Your Avatar]${(message.author.displayAvatarURL)({ dynamic: true })}`));
         }
 
         const avatar_list = message.mentions.users.map(user => {
-            return `**${user.username}'s Avatar: ** ${user.displayAvatarURL({ dynamic: true })}`;
+            return(new Discord.MessageEmbed() .setDescription(`**${user.username}**'s [Avatar link]${(user.displayAvatarURL)({ dynamic: true })}`));
         });
 
         message.channel.send(avatar_list);
---
+++ commands/suggetions.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'suggestions',
+    aliases: ['suggest', 'suggestion', 'اقتراح'],
+    permissions: [],
+    description: 'creates a suggestion!',
+    execute(message, args, client){
+        const channel = message.guild.channels.cache.find(c => c.name === '「📨」suggest');
+        if(!channel) return message.channel.send('there is no suggestion channel');
+
+        let messageArgs = args.join(' ');
+        const embed = new discord.MessageEmbed()
+        .setColor('#00CFC9')
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        .setDescription(messageArgs);
+
+        channel.send(embed).then((msg) =>{
+            message.delete();
+        }).catch((err)=>{
+            throw err;
+        });
+    }
+}
