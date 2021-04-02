75ac27cf19021d416b2912494d7da85409348086
Tanya575 <omankingomanking@gmail.com>
2021-04-02 19:58:13 +0400

    ko

--- commands/unlock.js
+++ commands/unlock.js
@@ -5,13 +5,13 @@ module.exports = {
     permissions: ["MANAGE_CHANNELS"],
     description: "this command locked channel!",
     async execute(client, message, args) {
-        let msg = await message.channel.send("channel has been unlocked")
+        let msg = await message.channel.send("<:gears:819943211530977350> just a second")
 
         try {
             message.channel.updateOverwrite(message.guild.roles.cache.find(e => e.name.toLowerCase().trim() == "@everyone"), {
                 SEND_MESSAGES: true,
             })
-            msg.edit("channel unlocked")
+            msg.edit(`<a:yes_1:823243336664088616> <@${channel.id}> has been succsesfully **unlocked`)
         }catch(e) {
             console.log(e)
         }
