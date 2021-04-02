339971ce09903305de1a2ba064217bca07264d36
Tanya575 <omankingomanking@gmail.com>
2021-04-03 00:18:36 +0400

    m

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'avatar',
     aliases: ['pfp', 'صور', 'v'],
-    permissions: [""],
     description: "this command give you your avatar(profile icon)",
     execute(client, message, args){
         message.channel.id !=='827127242005020702'
