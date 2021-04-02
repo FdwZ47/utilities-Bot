045bdf531714ebade6765a1384da2c40047ad1d0
Tanya575 <omankingomanking@gmail.com>
2021-04-03 01:53:36 +0400

    io

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,22 +6,20 @@ module.exports = {
     permissions: ["USE_EXTERNAL_EMOJIS"],
     description: "this command give you your avatar(profile icon)",
     execute(client, message, args){
-        //message.channel.id !=='827127242005020702'
+        if (message.channel.id !=='827127242005020702') return;
         if (args[0]) {
             const user = message.mentions.users.first();
             if (!user) return message.channel.send('please make sure to mention the user after command **don t Just use ID**');
 
-            const otherIconEmbed = new Discord.RIchEmbed()
+            const otherIconEmbed = new Discord.MessageEmbed()
             .setTitle(`${user.username}'s avatar`)
-            .setColor('#8FA0EA')
             .setImage(user.displayAvatarURL);
 
             return message.channel.send(otherIconEmbed).catch(err => console.log(err));
         }
 
-        const myIconEmbed = new Discord.RichEmbed()
+        const myIconEmbed = new Discord.MessageEmbed()
         .setTitle(`${message.author.username}'s avatar`)
-        .setColor('#8FA0EA')
         .setImage(message.author.displayAvatarURL);
 
         return message.channel.send(myIconEmbed).catch(err => console.log(err));
