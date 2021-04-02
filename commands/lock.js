ef15dc3bbca4d8c38d2891a87e22358ca99a744f
Tanya575 <omankingomanking@gmail.com>
2021-04-02 20:16:47 +0400

    lo

--- commands/lock.js
+++ commands/lock.js
@@ -6,13 +6,13 @@ module.exports = {
     description: "this command locked channels!",
     async execute(client, message, args) {
 
-        let msg = await message.channel.send(`<:gears:819943211530977350> just a second`)
+        let msg = await message.channel.send("<:gears:819943211530977350> just a second")
 
         try {
             message.channel.updateOverwrite(message.guild.roles.cache.find(e => e.name.toLowerCase().trim() == "@everyone"), {
                 SEND_MESSAGES: false,
             })
-            msg.edit(`<a:yes_1:823243336664088616> <@${channel.id}> has been succsesfully locked`)
+            msg.edit("<a:yes_1:823243336664088616> channel has been succsesfully **locked**")
     
         }catch(e) {
             console.log(e)
