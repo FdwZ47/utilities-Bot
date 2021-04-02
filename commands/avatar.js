ce1a33a0834e24c6fd72ae1b0368c51a43416dab
Tanya575 <omankingomanking@gmail.com>
2021-04-03 00:08:51 +0400

    uwu

---
+++ commands/avatar.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'avatar',
+    aliases: ['pfp', 'صور', 'v'],
+    permissions: [""],
+    description: "this command give you your avatar(profile icon)",
+    execute(client, message, args){
+        message.channel.id !=='827127242005020702'
+        if (args[0]) {
+            const user = message.mentions.users.first();
+            if (!user) return message.channel.send('please make sure to mention the user after command **don t Just use ID**');
+
+            const otherIconEmbed = new Discord.RichEmbed()
+            .sertTitle(`**${user.username}'s avatar**`)
+            .setcolor(RANDOM)
+            .setImage(user.displayAvatarURL);
+
+            return message.channel.send(otherIconEmbed).catch(err => console.log(err));
+        }
+
+        const myIconEmbed = new Discord.RichEmbed()
+        .sertTitle(`**${message.author.username}'s avatar**`)
+        .setcolor(RANDOM)
+        .setImage(message.author.displayAvatarURL);
+
+        return message.channel.send(myIconEmbed).catch(err => console.log(err));
+    }
+}
