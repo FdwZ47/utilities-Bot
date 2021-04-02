23c3c118b37ef285174a841d9c9f9bdf77cbc578
Tanya575 <omankingomanking@gmail.com>
2021-04-02 12:41:31 +0400

    ok

--- commands/embed.js
+++ commands/embed.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 module.exports = {
     name: "embed",
-    aliases: ['em', 'make'],
+    aliases: ['em', 'make', 'add'],
     permissions: ["ADMINISTRATOR"],
     description: "make embed",
 
