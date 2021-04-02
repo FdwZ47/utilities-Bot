f67452efc55bb841f161fb9c822b626e12fc6fe7
Tanya575 <omankingomanking@gmail.com>
2021-04-02 14:04:46 +0400

    tyo

---
+++ commands/botactivity.js
@@ -0,0 +1,39 @@
+module.exports = {
+    name: 'bot activity',
+    aliases: ['botact'],
+    description: "this command change the bot activity",
+    execute(client, message, args){
+        if(message.author.id != 755767643331362916) return message.channel.send("<:gears:819943211530977350> **only my developer** can run this command");
+        //here you tell the bot to choose the kind of activity
+if (args[0] === "playing"){
+    types = 0
+} else if (args[0] === "streaming") {
+    types = 1
+} else if (args[0] === "listening") {
+    types = 2
+} else if (args[0] === "watching") {
+    types = 3
+} else if (args[0] === "competing") {
+    types = 5
+} else if (args[0] === "reset") {
+
+    client.user.setActivity(`commands`, {type:"LISTENING"}) //you can change that to whatever you like
+
+    return message.channel.send('<a:yes_1:823243336664088616> Status changed succesfully')
+
+} else {
+    return message.channel.send('Invalid activity type.')
+}
+//here you tell the bot what the activity is
+    args.shift()
+    content = args.join(' ')
+    client.user.setPresence({
+        activity: {
+            name: content,
+            type: types
+        }
+    })
+    message.channel.send('<:gears:819943211530977350> Status changed succesfully')
+    }
+
+    }
