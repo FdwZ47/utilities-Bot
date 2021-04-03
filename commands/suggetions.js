06417d7848a30e9fb9e6f63c530e3f56cd4e1df1
Tanya575 <omankingomanking@gmail.com>
2021-04-03 19:38:12 +0400

    k

--- commands/suggetions.js
+++ commands/suggetions.js
@@ -1,25 +1,25 @@
-const Discord = require('discord.js');
-module.exports = {
-    name: 'suggestions',
-    aliases: ['suggest', 'suggestion', 'اقترح'],
-    permissions: [],
-    description: 'creates a suggestion!',
-    execute(client, message, args){
-        const channel = message.guild.channels.cache.find(c => c.name === '「📨」suggest');
-        if(!channel) return message.channel.send('suggestions channel does not exist!');
+//const Discord = require('discord.js');
+//module.exports = {
+  //  name: 'suggestions',
+    ///aliases: ['suggest', 'suggestion', 'اقترح'],
+//    permissions: [],
+  //  description: 'creates a suggestion!',
+   // execute(client, message, args){
+  //      const channel = message.guild.channels.cache.find(c => c.name === '「📨」suggest');
+     //   if(!channel) return message.channel.send('suggestions channel does not exist!');
 
-        let messageArgs = args.join(' ');
-        const embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-        .setDescription(messageArgs);
+ //       let messageArgs = args.join(' ');
+   //     const embed = new Discord.MessageEmbed()
+        //.setColor('RANDOM')
+     //   .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+       // .setDescription(messageArgs);
 
-        channel.send(embed).then((msg) =>{
-            msg.react('<a:yes:822217053863673856>');
-            msg.react('<a:no:826693930678616104>');
-            message.delete();
-        }).catch((err)=>{
-            throw err;
-        });
-    }
-}
+//        channel.send(embed).then((msg) =>{
+  //          msg.react('<a:yes:822217053863673856>');
+    //        msg.react('<a:no:826693930678616104>');
+      //      message.delete();
+      //  }).catch((err)=>{
+       //     throw err;
+       // });
+   // }
+//}
