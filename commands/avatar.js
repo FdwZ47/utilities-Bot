f74941e6b7d541834804a2444ae3725a635910dc
Tanya575 <omankingomanking@gmail.com>
2021-04-03 11:27:43 +0400

    lol

--- commands/avatar.js
+++ commands/avatar.js
@@ -12,10 +12,10 @@ module.exports = {
             if (!user) return message.channel.send('please make sure to mention the user after command **don t Just use ID**');
 
 
-            return message.channel.send(new Discord.MessageEmbed() .setTitle(`${user.username}'s avatar`) .setDescription(`[Avatar link](${user.avatarURL()})`) .setImage(user.displayAvatarURL({ dynamic: true, Image_size: 2048 })));
+            return message.channel.send(new Discord.MessageEmbed() .setTitle(`${user.username}'s avatar`) .setDescription(`[Avatar link](${user.avatarURL()})`) .setImage(user.displayAvatarURL({ dynamic: true, size: 2048 })) .setColor('#8FA0EA'));
         }
        
 
-        return message.channel.send(new Discord.MessageEmbed() .setTitle(`${message.author.username}'s avatar`) .setDescription(`[Avatar link](${message.author.avatarURL()})`) .setImage(message.author.displayAvatarURL({ dynamic: true, Image_size: 2048 })));
+        return message.channel.send(new Discord.MessageEmbed() .setTitle(`${message.author.username}'s avatar`) .setDescription(`[Avatar link](${message.author.avatarURL()})`) .setImage(message.author.displayAvatarURL({ dynamic: true, size: 2048 })) .setColor('#8FA0EA'));
     }
 }
