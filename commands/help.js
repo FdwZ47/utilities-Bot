867c53747dc33ae0d13e740bff9bb377d3525312
Tanya575 <omankingomanking@gmail.com>
2021-04-07 09:11:24 +0400

    mm

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+//const pagination = require('discord.js-pagination');
+//const Discord = require('discord.js');
+
+//module.exports = {
+ //   name: 'help',
+   // aliases: ['helpme'],
+//    permissions: ["USE_EXTERNAL_EMOJIS"],
+  //  description: "this command give you all the bot commands",
+    //execute(client, message, args, Discord) {
+
+      //  const info = new Discord.MessageEmbed()
+       // .setTitle('information')
+        //.addField('**prefix**', 'Bot prefix = `>`')
+       // addField('')
+
+    //}
+//}
--- commands/unmute.js
+++ commands/unmute.js
@@ -15,7 +15,7 @@ module.exports = {
             memberTarget.roles.remove(muteRole.id);
             message.channel.send(new Discord.MessageEmbed() .setDescription(`<a:yes_1:823243336664088616> <@${memberTarget.user.id}> **has been unmuted**`) .setColor('#93BFE6'));
         } else{
-            message.channel.send('<:idk:822217580567592991> I cannot find that member!');
+            message.channel.send('<:idk:822217580567592991> I cannot find this member');
         }
     }
 }
