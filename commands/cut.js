4754635188865562baa7f69b51b1ac510ce48a20
Tanya575 <omankingomanking@gmail.com>
2021-04-09 15:45:20 +0400

    s

--- commands/cut.js
+++ commands/cut.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 module.exports = {
   name: 'كت',
-  aliases: ['c'],
-  permissions: [],
+  aliases: ['ك'],
+  permissions: ["SEND_TTS_MESSAGES"],
   description: 'cut command',
   async execute(client, message, args, Discord) {
-    if (!member) member.roles.cache.has('827273929818898443') return;
+   // if (!member) member.roles.cache.has('827273929818898443') return;
  //  if (message.channel.id !== '796211852920487987') return message.channel.send('you can not use this command here')
    // if (!args[0]) return message.channel.send('Please ask a full question!'); // return if no question is commenced
     const replies = ['اغنية عالقة معك ذي اليومين', 'وش تفضل النوم او الاكل او الدراسة', 'كلام ودك انه يوصل لشخص معين', 'اكله مستحيل تاكلها؟', 'افضل صديق لك يبدأ بأي حرف؟', 'حاجة صعب تتغير فيك' , 'اذا احد كسر جوالك وش بتعمل', 'اكثر كلمة تنقال لك', 'حاجة ما تتقبل انك تمزح بيها او غيرك يمزح بيها', 'افضل انجازاتك ذا الاسبوع', 'منشن شخص ودك تعطيه بلوك ابدي', 'وش اكثر كلمة تقولها باليوم ', 'افضل لون عندك وش هو', 'كذبة صدقتها و انت صغير', 'اكثر حاجة تعملها وقت فراغك', 'كلمة توصف شعورك الحين', 'برأيك ايش افضل طريقة علشان تفتح سالفة مع واحد ما تعرفه ولا هو يعرفك', 'مادة دراسية ودك انها تصير مو موجودة بالمنهج للأبد', 'منشن شخص عنده سالفة حلوة']; // random responses
