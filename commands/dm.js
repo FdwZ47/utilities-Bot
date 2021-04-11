8c4c99c70c862e4baeebb9ab12633b3af659761f
Tanya575 <omankingomanking@gmail.com>
2021-04-12 01:18:28 +0400

    j

---
+++ commands/dm.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'dm',
+    aliases: [],
+    permissions: [],
+    description: "this command dm the member",
+    async execute(client, message, args, Discord){
+        if(message.author.id != 755767643331362916) return message.channel.send(new Discord.MessageEmbed() .setTitle('<:NO:828567406347419669> For developer') .setDescription('only my developer can run this command') .setColor('#9542FF'));
+
+        const user = 
+        message.mentions.users.first() ||
+        message.guild.members.cache.get(args[0])?.user;
+
+        const str = args.slice(1).join(" ");
+        if (message.content.incloudes("-a")) {
+            user.send(str.replace("-a", ""));
+        } else {
+            user.send(`${str}`);
+        }
+    }
+}    
