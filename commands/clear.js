23630815c1f972067e415d3291745c7154d684a3
Tanya575 <omankingomanking@gmail.com>
2021-04-11 16:25:21 +0400

    j

--- commands/clear.js
+++ commands/clear.js
@@ -6,15 +6,11 @@ module.exports = {
     description: "clear messages!",
     async execute(client, message, args, Discord) {
 
-        const amount = new Discord.MessageEmbed()
-        .setdescription('<a:srry:825769123686252545> please enter the amount of messages thet you want to clear')
-        .setColor('RED')
-        if(!args[0]) return message.channel.send(amount)
-        if(isNaN(args[0])) return message.channel.send(new Discord.MessageEmbed() .setdescription('please enter a real number') .setColor('RED'));
+        if(!args[0]) return 
+        if(isNaN(args[0])) return message.reply('please enter a real number')
 
-        if(args[0] > 100) return message.channel.send(new Discord.MessageEmbed() .setdescription('you can not delete more than 100 messages you only can between 1 - 100') .setColor('RED'));
-        if(args[0] < 1) return message.channel.send(new Discord.MessageEmbed() .setdescription('you must delete atleaste one message') .setColor('RED'));
-
+        if(args[0] > 100) return message.reply('you can not delete more than 100 messages you only can between 1 - 100')
+        if(args[0] < 1) return message.reply('you must delete atleaste one message') 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
             message.channel.bulkDelete(messages);
         });
