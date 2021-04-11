803090582f0c526132610cffcd827bf7daf0f968
Tanya575 <omankingomanking@gmail.com>
2021-04-11 16:16:14 +0400

    j

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,11 @@ module.exports = {
     permissions: ["MANAGE_MESSAGES"],
     description: "clear messages!",
     async execute(client, message, args, Discord) {
-        if(!args[0]) return message.channel.send(new Discord.MessageEmbed() .setdescription('<a:srry:825769123686252545> please enter the amount of messages thet you want to clear') .setColor('RED'));
+
+        const amount = new Discord.MessageEmbed()
+        .setdescription('<a:srry:825769123686252545> please enter the amount of messages thet you want to clear')
+        .setColor('RED')
+        if(!args[0]) return message.channel.send(amount)
         if(isNaN(args[0])) return message.channel.send(new Discord.MessageEmbed() .setdescription('please enter a real number') .setColor('RED'));
 
         if(args[0] > 100) return message.channel.send(new Discord.MessageEmbed() .setdescription('you can not delete more than 100 messages you only can between 1 - 100') .setColor('RED'));
