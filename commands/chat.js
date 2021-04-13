6d707e5f089bab509dbb802667f08b6f43f8f1c2
Tanya575 <omankingomanking@gmail.com>
2021-04-13 14:42:14 +0400

    j

--- commands/chat.js
+++ commands/chat.js
@@ -4,16 +4,19 @@ module.exports = {
     aliases: [],
     permissions: [],
     description: 'send messages in channel',
-    execute(client, message, args, Discord){
+    async execute(client, message, args, Discord){
         if(message.author.id != 755767643331362916) return message.channel.send(new Discord.MessageEmbed() .setTitle('<:NO:828567406347419669> For developer') .setDescription('only my developer can run this command') .setColor('#9542FF'));
   
+        let channels = message.guild.channels.cache.get(args[0])
+        if(!channels) return message.channel.send('please specific a channel')
 
-        let channels = message.mentions.channels.first();
-        let messageArgs = args.join(' ');
+       // let channels = message.mentions.channels.first()
+        let message = message.content.join("")
         
-        message.guild.channels.find(c => c.id == channels.id).send(messageArgs);
+        message.guild.channels.find(c => c.id == channels.id).send(message);
+
+        channels.channel.send(message)
         
-        if(!channels) return message.channel.send('please specific a channel');
 
     }
 }
