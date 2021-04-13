97eb4337bd2a53de5ec5ff44db585b02721d1644
Tanya575 <omankingomanking@gmail.com>
2021-04-13 15:34:39 +0400

    h

--- commands/chat.js
+++ commands/chat.js
@@ -7,14 +7,14 @@ module.exports = {
     async execute(client, message, args, Discord){
         if(message.author.id != 755767643331362916) return message.channel.send(new Discord.MessageEmbed() .setTitle('<:NO:828567406347419669> For developer') .setDescription('only my developer can run this command') .setColor('#9542FF'));
   
-        let rchannel = message.guild.channels.cache.get(args[0])
+        const rchannel = message.guild.channels.cache.get(args[0])
         if(!rchannel) return message.channel.send('please specific a channel')
 
        // let channels = message.mentions.channels.first()
-        const msg = message.content.split().join("")
+        const msg = message.content.join("")
         if(!msg) return message.channel.send('please input something')
         
-        rchannel.channel.send(msg)
+        channel.send(msg)
 
       //message.channel.send(msg)
         
