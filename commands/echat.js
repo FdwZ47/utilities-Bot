c67b7e0793b3dff177b37f401112dbfd86ec055c
Tanya575 <omankingomanking@gmail.com>
2021-04-13 20:31:11 +0400

    j

---
+++ commands/echat.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'echat',
+    aliases: [],
+    permissions: [],
+    description: 'send messages in channel',
+    async execute(client, message, args, Discord){
+        if(message.author.id != 755767643331362916) return message.channel.send(new Discord.MessageEmbed() .setTitle('<:NO:828567406347419669> For developer') .setDescription('only my developer can run this command') .setColor('#9542FF'));
+  
+        const rchannel = message.mentions.channels.first();
+        if(!rchannel) return message.channel.send('please specific a channel');
+
+        if(!args[1]) return message.channel.send('please input something');
+
+
+        const eEmbed = new Discord.MessageEmbed()
+        .setDescription(args.slice(1).join(" "))
+        .setColor('RANDOM')
+       // let channels = message.mentions.channels.first()
+       // const msg = args.join(" ")
+       // if(!msg) return message.channel.send('please input something')
+        
+       rchannel.send(eEmbed)
+
+      //message.channel.send(msg)
+        
+
+    }
+}
