5915cf9a16d8e80443ae2900b71d9e62f0eea7bd
Tanya575 <omankingomanking@gmail.com>
2021-04-18 12:08:30 +0400

    j

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-client.on('guildMemberAdd', guildMember => {
-    guildMember.guild.channels.cache.get('768676161953333279').send(`**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${guildMember.user.id}>`)
-})
+// client.on('guildMemberAdd', guildMember => {
+//     guildMember.guild.channels.cache.get().send(`**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${guildMember.user.id}>`)
+// })
 
 
 // const welcome = require('./welcome')
