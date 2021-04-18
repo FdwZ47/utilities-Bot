1aeb77c116e178b0fdf2cb7ae606cc166a8a0948
Tanya575 <omankingomanking@gmail.com>
2021-04-18 10:47:07 +0400

    gff

--- welcome.js
+++ welcome.js
@@ -1,13 +1,14 @@
 module.exports = (client) => {
-    const channelId = '806447826548162580' 
-    const rules = '768680803940040725' 
-    const info = '772065150957125642'
+   // const channelId = '806447826548162580' 
+  //  const rules = '768680803940040725' 
+    //const info = '772065150957125642'
   
-    client.on('guildMemberAdd', (member) => {
-      const message = `**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${member.id}>`
+    client.on('guildMemberAdd', (guildMember) => {
+        guildMember.guild.channels.cache.get('806447826548162580').send(`**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${guildMember.user.id}>`)
+    //   const message = `**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${member.id}>`
   
-      const channel = member.guild.channels.cache.get(channelId)
-      channel.send(message)
+    //   const channel = member.guild.channels.cache.get(channelId)
+    //   channel.send(message)
     })
   }
 
@@ -16,6 +17,7 @@ module.exports = (client) => {
 
 
 
+  `**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${member.id}>`
 
 
 
