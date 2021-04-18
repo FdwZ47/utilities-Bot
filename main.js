9191cacca61f54b8bb03b134bd3bf78b44a601f6
Tanya575 <omankingomanking@gmail.com>
2021-04-18 11:58:31 +0400

    g

--- main.js
+++ main.js
@@ -11,10 +11,15 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-const welcome = require('./welcome')
+client.on('guildMemberAdd', guildMember => {
+    guildMember.guild.channels.cache.get('806447826548162580').send(`**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${guildMember.user.id}>`)
+})
 
 
-welcome(client)
+// const welcome = require('./welcome')
+
+
+// welcome(client)
 
 
 client.login(process.env.DISCORD_TOKEN);
