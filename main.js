bf4de08797b5dfdaafe745588a4d9cb5ecc8cb1c
Tanya575 <omankingomanking@gmail.com>
2021-04-18 12:02:22 +0400

    h

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ client.events = new Discord.Collection();
 })
 
 client.on('guildMemberAdd', guildMember => {
-    guildMember.guild.channels.cache.get('806447826548162580').send(`**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${guildMember.user.id}>`)
+    guildMember.guild.channels.cache.get('768676161953333279').send(`**هلا والله نورت سيرفر ENJOY <a:ENJOY_190:808366642634817617>**\n**لا تنسى تشيك القوانين <#768680803940040725> و شرح السيرفر <#772065150957125642> قبل كل حاجة <a:ENJOY_44:786515261012181012>**\n<@${guildMember.user.id}>`)
 })
 
 
