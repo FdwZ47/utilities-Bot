3e64a5a181beb8abad94d8060b377ec7b37b14a7
Tanya575 <omankingomanking@gmail.com>
2021-04-22 07:00:45 +0400

    nnn

---
+++ commands/role.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'role',
+    aliases: ['r'],
+    permissions: ["MANAGE_ROLES"],
+    description: "this command give a role to the member!",
+    execute(client, message, args, Discord){
+        
+        const Target = message.mentions.users.first() ||
+        message.guild.members.cache.get(args[0])?.user;
+
+       if(!Target) return;
+
+       args.shift()
+
+       const rolename = args.join(' ')
+       const { guild } = message
+
+       const role = guild.roles.cache.find((role) => {
+           return role.name === rolename
+       })
+        const member = guild.members.cache.get(Target.id)
+        member.roles.add(role)
+        message.channel.send(new Discord.MessageEmbed() .setDescription(`<a:yes:822217053863673856> Changed roles for <@${memberTarget.user.id}>, +${rolename} `) .setColor('RANDOM'));
+        
+        if (member.roles.cache.get(role.id)) {
+            member.roles.remove(role)
+            message.channel.send(new Discord.MessageEmbed() .setDescription(`<a:yes:822217053863673856> Changed roles for <@${memberTarget.user.id}>, -${rolename} `) .setColor('RANDOM'));
+        
+        }
+    },
+}
